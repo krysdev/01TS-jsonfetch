a9e6ade70d25288da1d28189a85a16f7bfc034f6
KrysDev <webdevkrys@gmail.com>
2023-04-23 22:09:22 +0100

    x

---
+++ index.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+
+axios.get(url).then ((response) => {
+  // console.log(response.data)
+  const todo = response.data
+  
+
+})
